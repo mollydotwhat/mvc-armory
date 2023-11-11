08be9a90fb22664fb73a1804e0307b5759dd415a
seanwsutter <seanwsutter@gmail.com>
2023-11-10 21:14:54 -0600

    revert drag

--- assets/js/dragAndDrop4.js
+++ assets/js/dragAndDrop4.js
@@ -72,80 +72,94 @@
 // })
 // -------------------------------------------
 // Usable Code Below
-// -------------------------------------------
 
-
-// Functions that make items draggable
-function setupDragListeners() {
-  const items = document.querySelectorAll(".item")
-  items.forEach(item => {
-    switch (true) {
-      case item.classList.contains('head'): {
-          addStartEndListeners(item, 'head')
-        break;
-      }
-      case item.classList.contains('torso'): {
-        addStartEndListeners(item, 'torso')
-        break;
-      }
-      case item.classList.contains('leg'): {
-        addStartEndListeners(item, 'leg')
-        break;
-      }
-      case item.classList.contains('feet'): {
-        addStartEndListeners(item, 'feet')
-        break;
-      }
+$(function () {
+  $("#draggable, #draggable-nonvalid").draggable();
+  $("#droppable").droppable({
+    accept: "#draggable",
+    classes: {
+      "ui-droppable-active": "ui-state-active",
+      "ui-droppable-hover": "ui-state-hover"
+    },
+    drop: function (event, ui) {
+      $(this)
+        .addClass("ui-state-highlight")
+        .find("p")
+        .html("Dropped!");
     }
-  })
-}
+  });
+});
+// Functions that make items draggable
+// function setupDragListeners() {
+//   const items = document.querySelectorAll(".item")
+//   items.forEach(item => {
+//     switch (true) {
+//       case item.classList.contains('head'): {
+//         addStartEndListeners(item, 'head')
+//         break;
+//       }
+//       case item.classList.contains('torso'): {
+//         addStartEndListeners(item, 'torso')
+//         break;
+//       }
+//       case item.classList.contains('leg'): {
+//         addStartEndListeners(item, 'leg')
+//         break;
+//       }
+//       case item.classList.contains('feet'): {
+//         addStartEndListeners(item, 'feet')
+//         break;
+//       }
+//     }
+//   })
+// }
 
-function addStartEndListeners(item, itemType) {
-  item.addEventListener('dragstart', () => {
-    item.classList.add(`${itemType}_dragging`)
-  })
-  item.addEventListener('dragend', () => {
-    item.classList.remove(`${itemType}_dragging`)
-  })
-}
+// function addStartEndListeners(item, itemType) {
+//   item.addEventListener('dragstart', () => {
+//     item.classList.add(`${itemType}_dragging`)
+//   })
+//   item.addEventListener('dragend', () => {
+//     item.classList.remove(`${itemType}_dragging`)
+//   })
+// }
 
-setupDragListeners()
+// setupDragListeners()
 
 // Functions that make slots accept certain items
-function setUpSlotListeners() {
-  const slots = document.querySelectorAll(".slot")
-  slots.forEach(slot => {
-    switch (true) {
-      case slot.id === 'head_slot': {
-        addDragOverListener(slot, 'head')
-        break;
-      }
-      case slot.id === 'torso_slot': {
-        addDragOverListener(slot, 'torso')
-        break;
-      }
-      case slot.id === 'leg_slot': {
-        addDragOverListener(slot, 'leg')
-        break;
-      }
-      case slot.id === 'feet_slot': {
-        addDragOverListener(slot, 'feet')
-        break;
-      }
-    }
-  })
-}
+// function setUpSlotListeners() {
+//   const slots = document.querySelectorAll(".slot")
+//   slots.forEach(slot => {
+//     switch (true) {
+//       case slot.id === 'head_slot': {
+//         addDragOverListener(slot, 'head')
+//         break;
+//       }
+//       case slot.id === 'torso_slot': {
+//         addDragOverListener(slot, 'torso')
+//         break;
+//       }
+//       case slot.id === 'leg_slot': {
+//         addDragOverListener(slot, 'leg')
+//         break;
+//       }
+//       case slot.id === 'feet_slot': {
+//         addDragOverListener(slot, 'feet')
+//         break;
+//       }
+//     }
+//   })
+// }
 
-function addDragOverListener(slot, slotType) {
-  slot.addEventListener('dragover', (event) => {
-    try {
-      const item = document.querySelector(`.${slotType}_dragging`)
+// function addDragOverListener(slot, slotType) {
+//   slot.addEventListener('dragover', (event) => {
+//     try {
+//       const item = document.querySelector(`.${slotType}_dragging`)
 
-      slot.appendChild(item)
-      event.preventDefault()
-    } catch (error) {
-    }
-  })
-}
+//       slot.target.appendChild(item)
+//       event.preventDefault()
+//     } catch (error) {
+//     }
+//   })
+// }
 
-setUpSlotListeners()
+// setUpSlotListeners()
