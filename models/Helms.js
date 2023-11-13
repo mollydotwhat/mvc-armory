cb0ff07d031a47713ea87d9fa0f6ee542a3f9699
FranklinBrad <franklin1jbradley@gmail.com>
2023-11-13 10:30:39 -0600

    added models

--- models/Helms.js
+++ models/Helms.js
@@ -1,1 +1,66 @@
 // id, stats, img link.
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Helms extends Model {}
+
+Helms.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    item_type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    health:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    armor:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    attack:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    defense:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    speed:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    link_to_photo:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    material: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+ 
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'painting',
+  }
+);
+
+module.exports = Helms;
