7483e3bf27640cbf8e18328320335da43f4c9260
FranklinBrad <franklin1jbradley@gmail.com>
2023-11-14 10:12:10 -0600

    added seeds

--- models/Item.js
+++ models/Item.js
@@ -1,20 +1,56 @@
+// type/slot? id, name
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 // type/slot? id, name
 
 class Item extends Model { };
 Item.init(
-{
+  {
     id: {
-
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-
-},
-{
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    item_type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    health:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    armor:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    attack:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    defense:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    speed:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    link_to_photo:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+ 
+  },
+  {
     sequelize,
-    timestamps: true,
+    freezeTableName: true,
     underscored: true,
-    modelName: 'Item'
+    modelName: 'Item',
   }
 );
 
