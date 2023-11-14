b2cb7c367f3b3a518efee64fcc87fe86a7d9d9cf
molly <molly.salzberger@gmail.com>
2023-11-14 12:47:56 -0600

    get item by id, get item by type

--- controllers/api/item.api.routes.js
+++ controllers/api/item.api.routes.js
@@ -11,9 +11,59 @@ router.get("/:id", async (req, res) => {
     }
   })
 // GET request by item_type = helmet ('/helmet')
+router.get("/helmet", async (req, res) => {
+    try {
+        const payload = await Item.findALL({ where : { item_type: 'helmet'}});
+        res.status(200).json({ status: "success", payload });
+    } catch(err){
+        res.status(500).json({ status: "error", payload: err.message });
+    }
+})
 // GET request by item_type = torso ('/torso')
+router.get("/torso", async (req, res) => {
+    try {
+        const payload = await Item.findALL({ where : { item_type: 'torso'}});
+        res.status(200).json({ status: "success", payload });
+    } catch(err){
+        res.status(500).json({ status: "error", payload: err.message });
+    }
+})
 // etc... with weapon, boot, gem, and shield as well
+router.get("/weapon", async (req, res) => {
+    try {
+        const payload = await Item.findALL({ where : { item_type: 'weapon'}});
+        res.status(200).json({ status: "success", payload });
+    } catch(err){
+        res.status(500).json({ status: "error", payload: err.message });
+    }
+})
 
+router.get("/boot", async (req, res) => {
+    try {
+        const payload = await Item.findALL({ where : { item_type: 'boot'}});
+        res.status(200).json({ status: "success", payload });
+    } catch(err){
+        res.status(500).json({ status: "error", payload: err.message });
+    }
+})
+
+router.get("/gem", async (req, res) => {
+    try {
+        const payload = await Item.findALL({ where : { item_type: 'gem'}});
+        res.status(200).json({ status: "success", payload });
+    } catch(err){
+        res.status(500).json({ status: "error", payload: err.message });
+    }
+})
+
+router.get("/shield", async (req, res) => {
+    try {
+        const payload = await Item.findALL({ where : { item_type: 'helmet'}});
+        res.status(200).json({ status: "success", payload });
+    } catch(err){
+        res.status(500).json({ status: "error", payload: err.message });
+    }
+})
 // router.get('/', async (req, res) => {
 //   const userData = await Item.findAll().catch((err) => {
 //     res.json(err);
