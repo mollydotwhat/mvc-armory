fa00161b40c1c5e4755932efc04549ea90a89982
FranklinBrad <franklin1jbradley@gmail.com>
2023-11-14 12:54:43 -0600

    added seeds.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,20 +1,29 @@
 // TODO: Update this file
-
+const sequelize = require('../config/connection');
+const { Item, User } = require('../models');
 
 // const sequelize = require('../config/connection');
 // const { User } = require('../models');
 
-// const userData = require('./userData.json');
+ const userSeedData = require('./userData.json');
+ const itemSeedData = require('./itemSeeds.json');
+ 
 
-// const seedDatabase = async () => {
-//   await sequelize.sync({ force: true });
+ const seedDatabase = async () => {
+   await sequelize.sync({ force: true });
 
-//   await User.bulkCreate(userData, {
-//     individualHooks: true,
-//     returning: true,
-//   });
 
-//   process.exit(0);
-// };
 
-// seedDatabase();
+   await User.bulkCreate(userSeedData, {
+     individualHooks: true,
+     returning: true,
+   });
+   await Item.bulkCreate(itemSeedData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+   process.exit(0);
+ };
+
+ seedDatabase();
