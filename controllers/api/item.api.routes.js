c3f7e4ecce6f98a3e54abfb736bafb97c6821aa3
molly <molly.salzberger@gmail.com>
2023-11-14 12:31:40 -0600

    not sure if try/catch should go here? maybe it's overcomplicating. LOOKS like it'll work but i still have to test it with insomnia

--- controllers/api/item.api.routes.js
+++ controllers/api/item.api.routes.js
@@ -1,6 +1,19 @@
 const router = require('express').Router();
 const { Item } = require('../../models/Item');
 
+// GET request by id ('/:id')
+router.get("/:id", async (req, res) => {
+    try {
+      const payload = await Item.findByPk(req.params.id);
+      res.status(200).json({ status: "success", payload });
+    } catch (err) {
+      res.status(500).json({ status: "error", payload: err.message });
+    }
+  })
+// GET request by item_type = helmet ('/helmet')
+// GET request by item_type = torso ('/torso')
+// etc... with weapon, boot, gem, and shield as well
+
 // router.get('/', async (req, res) => {
 //   const userData = await Item.findAll().catch((err) => {
 //     res.json(err);
