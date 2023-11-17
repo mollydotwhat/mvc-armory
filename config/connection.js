e85b58f29b3675dd07cfcd79ef32460be94215cd
molly <molly.salzberger@gmail.com>
2023-11-16 18:22:36 -0600

    I now suspect a CAPITALIZATION ERROR. >:(

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
-const sequelize = process.env.JAWSDB_URL
-? new Sequelize(process.env.JAWSDB_URL)
-: new Sequelize(process.env.DB_NAME,
-  process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  }
-);
+const Sequelize = process.env.JAWSDB_URL
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize(process.env.DB_NAME,
+      process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+      }
+   );
